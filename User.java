2a993a61d0f64b0f5012e12686184ea858137961
Vivek Gupta <98093405+Vivekqc123@users.noreply.github.com>
2023-05-07 21:34:22 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,14 @@
+package com.example.b2b_20211467;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
